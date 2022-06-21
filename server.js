5cf3419771607e04c9a21ba31ebb5d799305dcba
John Emilio <jl.emilio1@gmail.com>
2022-06-21 16:21:15 -0400

    Changes to css

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+const cors = require("cors")
 const PORT = 8000
 require('dotenv').config()
 
@@ -16,11 +17,12 @@ MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
         db = client.db(dbName)
     })
 
-
+app.use(cors())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
 
 
 app.get('/', (req, res)=>{
